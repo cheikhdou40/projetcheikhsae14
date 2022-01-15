470edea54a9986c5d1193ee7f88d37e2cdd7e123
Cheikh Dieng <cheikhdou40@gmail.com>
2022-01-15 12:49:54 +0100

    initial commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+     /**
+	 * @Route("/",name="home") {
+     */
+    public function home () {
+	    return $this->render('blog/home.html.twig',[
+            
+        ]);	
+	}
+     /**
+	 * @Route("/loi",name="loisirs") {
+     */
+    public function loi () {
+	    return $this->render('blog/loisirs.html.twig',[
+            
+        ]);	
+	}
+     /**
+	 * @Route("/rv",name="RV") {
+     */
+    public function rv () {
+	    return $this->render('blog/RV.html.twig',[
+            
+        ]);	
+	}
+    /**
+	 * @Route("/conf",name="confirmationRV") {
+     */
+    public function conf () {
+	    return $this->render('blog/confirmationRV.html.twig',[
+            
+        ]);	
+	}
+    /**
+	 * @Route("/port",name="portfolio") {
+     */
+    public function port () {
+	    return $this->render('blog/portfolio.html.twig',[
+            
+        ]);	
+	}
+}
